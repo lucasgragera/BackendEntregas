5de329f77ed32a776eb6ed6b8821ca85bd068591
Lucas Gragera <lucasgragera1@gmail.com>
2023-12-14 19:59:22 -0300

    Estrategia de autenticación por terceros + JWT

--- primeraEntrega/src/daos/mongodb/models/user.model.js
+++ primeraEntrega/src/daos/mongodb/models/user.model.js
@@ -17,15 +17,21 @@ const userSchema = new Schema({
   age: {
     type: Number,
     required: true,
+    default: 0
   },
   password: {
     type: String,
     required: true,
+    default: ''
   },
   role: {
     type: String,
     default: "user",
   },
+  isGithub:{
+    type: Boolean,
+    default: false
+  }
 });
 
 export const UserModel = model("users", userSchema);
